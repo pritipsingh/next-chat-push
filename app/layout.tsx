462fd4763146311ca49f91d79f055eb77ad59b17
Pritipriya Singh <mail2pritipriya@gmail.com>
2023-12-04 01:59:49 +0530

    errors fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-// import './globals.css'
-import Providers from './provider'
-import "@rainbow-me/rainbowkit/styles.css";
+import './globals.css'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -18,9 +17,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>
+
           {children}
-        </Providers>
+  
         </body>
     </html>
   )
